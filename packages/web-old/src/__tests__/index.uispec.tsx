911377c26695fb7f7ce2814b7310dba59c4eb0a2
Max Rohde <1448524+mxro@users.noreply.github.com>
2026-03-25 14:37:42 +1100

    Removing reference project

--- packages/web-old/src/__tests__/index.uispec.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import Homepage from '../pages/index';
-
-test('Check App component render', () => {
-  render(<Homepage />);
-});
